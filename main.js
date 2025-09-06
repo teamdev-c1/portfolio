4bd2ad028458b47e484a014ef3d6d92da3c7c90d
Yamamoto 87 <cs@gmail.com>
2025-09-06 20:09:59 +0900

    change branch

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   const btn = document.getElementById("btn-click");
   const img = document.getElementById("img");
-
+  //modal
+  const modal = document.getElementById("js-modal");
+  const close_btn = modal.querySelector("js-close-btn");
+  const modal_img = modal.querySelector(".js-img");
+  const modal_text = modal.querySelector(".js-text");
+  const modal_lang = modal.querySelector(".js-lucky-lang");
+  //random preview
+  const result=[
+    {text:"",img:"",lang:""},
+    {text:"",img:"",lang:""},
+    {text:"",img:"",lang:""},
+    {text:"",img:"",lang:""},
+    {text:"",img:"",lang:""}
+  ];
+  //btnをクリックして、開いたとき
+  function open_modal(open){
+    modal_img.src = open.img;
+    modal.alt = `おみくじ：${open.text}`;
+    modal_text.textContent = open.text;
+    modal_lang = open.lang;
+    modal.classList.add("css-open");
+  }
+  //btnをクリックして、閉じた時
+  function close_modal(){
+    modal.classList.remove("css-open");
+  }
   btn.addEventListener("click", () => {
     img.classList.add("shaking");
 
