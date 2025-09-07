9e6b98bf706a310d60fd593a96fbcdd3f5c84397
yutoAb <yuto27abe@gmail.com>
2025-09-07 09:43:57 +0900

    画像のパスを修正

--- main.js
+++ main.js
@@ -14,12 +14,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const lucky_edit = modal.querySelector(".js-lucky-editor");
   //random pick
   const result=[
-    {text:"大吉",img:"/img/omikuji_daikichi.png",lang:"リリースしたソフトウェアにバグが見つかりますが、再現性が低いためユーザーも上司も誰ひとり気付かないでしょう。見なかったことにするのが吉。"},
-    {text:"中吉",img:"/img/omikuji_chuukichi.png",lang:"リリースしたソフトウェアに深刻なバグが見つかりますが、口八丁な営業のお陰で事無きを得るでしょう。ただし、後日それを理由に無茶振りされるおそれがあるので油断は禁物です。"},
-    {text:"小吉",img:"/img/omikuji_syoukichi.png",lang:"リリース前のソフトウェアにバグが見つかります。貴方のお勤め先が上流の企業ならばそれほど大きな問題にはなりませんが、三次請けよりも下流の場合はご愁傷様でございます。"},
-    {text:"吉",img:"/img/omikuji_kichi.png",lang:"開発の要件定義が綿飴のようにふわっふわです。手遅れにならないうちに手を打っておくのが吉。"},
-    {text:"凶",img:"/img/omikuji_kyou.png",lang:"前任者から引き継いだコードがスパゲティ状態です。残念ながらあなたの力ではどうにもならないので、せめて自分はこんなコードを書かないように今後の糧にしましょう。"},
-    {text:"末吉",img:"/img/omikuji_suekichi.png",lang:"リリース直前に致命的なバグが見つかるでしょう。お泊まりの準備をして出社するのが吉。"}
+    {text:"大吉",img:"./img/omikuji_daikichi.png",lang:"リリースしたソフトウェアにバグが見つかりますが、再現性が低いためユーザーも上司も誰ひとり気付かないでしょう。見なかったことにするのが吉。"},
+    {text:"中吉",img:"./img/omikuji_chuukichi.png",lang:"リリースしたソフトウェアに深刻なバグが見つかりますが、口八丁な営業のお陰で事無きを得るでしょう。ただし、後日それを理由に無茶振りされるおそれがあるので油断は禁物です。"},
+    {text:"小吉",img:"./img/omikuji_syoukichi.png",lang:"リリース前のソフトウェアにバグが見つかります。貴方のお勤め先が上流の企業ならばそれほど大きな問題にはなりませんが、三次請けよりも下流の場合はご愁傷様でございます。"},
+    {text:"吉",img:"./img/omikuji_kichi.png",lang:"開発の要件定義が綿飴のようにふわっふわです。手遅れにならないうちに手を打っておくのが吉。"},
+    {text:"凶",img:"./img/omikuji_kyou.png",lang:"前任者から引き継いだコードがスパゲティ状態です。残念ながらあなたの力ではどうにもならないので、せめて自分はこんなコードを書かないように今後の糧にしましょう。"},
+    {text:"末吉",img:"./img/omikuji_suekichi.png",lang:"リリース直前に致命的なバグが見つかるでしょう。お泊まりの準備をして出社するのが吉。"}
   ];
 
   const pick = (arr) => arr[Math.floor(Math.random() * arr.length)];
